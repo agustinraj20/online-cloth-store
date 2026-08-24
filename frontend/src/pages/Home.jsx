45d5807ad3a7381fa2e2f8299e0f78ce329e67f7
agustinRaj <agustinraj20@gmail.com>
2026-08-24 22:06:32 +0530

    Home page default update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,33 +7,12 @@ import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
-    <div className="w-full bg-white">
-
-      {/* Hero */}
-      <section className="mb-20">
-        <Hero />
-      </section>
-
-      {/* Latest Collection */}
-      <section className="mb-24">
-        <LatestCollection />
-      </section>
-
-      {/* Best Sellers */}
-      <section className="mb-24">
-        <BestSeller />
-      </section>
-
-      {/* Store Policy */}
-      <section className="mb-24 rounded-3xl bg-gray-50 py-12 px-4 sm:px-8">
-        <OurPolicy />
-      </section>
-
-      {/* Newsletter */}
-      <section className="mb-20">
-        <NewsletterBox />
-      </section>
-
+    <div>
+      <Hero />
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
