f63c5bba38539a243016cf145782ea4765041f17
agustinRaj <agustinraj20@gmail.com>
2026-08-24 10:06:52 +0530

    contact pg

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>Near Bombay Plaza <br /> Suite 350, Rajkot ,Gujarat India</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: DreamsClothing@gmail.com</p>
+          <p className=' text-gray-500'>Near Powr  Plaza <br /> Tamil Nadu ,Chennai India</p>
+          <p className=' text-gray-500'>Tel: 8489350439 <br /> Email: agustinraj20@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Dreams Clothing</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
